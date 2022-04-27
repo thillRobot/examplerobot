d1843462411d4ea674df552d3c935f5473ae2950
thill <thill@tntech.edu>
2022-04-26 23:28:36 -0500

    updated CMakeList.txt and package.xml

---
+++ docker/examplerobot_entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+source /opt/ros/noetic/setup.bash
+
+mkdir -p $EXBOT_WS/src 
+cd $EXBOT_WS/src
+git clone https://github.com/thillRobot/examplerobot_description.git -b noetic-devel 
+git clone https://github.com/thillRobot/examplerobot_gazebo.git -b noetic-devel 
+git clone https://github.com/thillRobot/examplerobot_moveit_config.git -b noetic-devel 
+git clone https://github.com/thillRobot/examplerobot.git -b noetic-devel
+cd $EXBOT_WS
+catkin_make 
+
+source $EXBOT_WS/devel/setup.bash
+
+source /usr/share/gazebo/setup.sh
+export GAZEBO_RESOURCE_PATH="$GAZEBO_RESOURCE_PATH/$EXBOT_WS/src/examplerobot_gazebo/worlds"
+
+exec "$@"
