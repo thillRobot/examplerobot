bcdfa8bc8d898a0ab9d87f20d829fdad4e4e462f
thill <thill@tntech.edu>
2022-05-05 09:16:22 -0500

    updated launch files

--- docker/examplerobot_interface.bash
+++ docker/examplerobot_interface.bash
@@ -11,9 +11,14 @@ then
     chmod a+r $XAUTH
 fi
 
-docker exec -it \
-    --env="DISPLAY=$DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --env="XAUTHORITY=$XAUTH" \
-    examplerobot_container \
-    roslaunch examplerobot examplerobot_interface.launch
+
+docker run -it \
+--name="examplerobot_container" \
+--env="DISPLAY=$DISPLAY" \
+--env="QT_X11_NO_MITSHM=1" \
+--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+--env="XAUTHORITY=$XAUTH" \
+--volume="$XAUTH:$XAUTH" \
+--runtime=nvidia \
+examplerobot \
+roslaunch examplerobot_interface examplerobot_interface.launch
